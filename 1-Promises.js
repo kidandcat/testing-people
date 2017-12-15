0833cae0528ff9ab5d8cece04531257fdb6395ae
Jairo Caro-Accino Viciana <jairo@galax.be>
2017-12-15 11:37:57 +0100

    Update 1-Promises.js

--- 1-Promises.js
+++ 1-Promises.js
@@ -2,9 +2,19 @@ new Promise((res, rej) => {
   res("aaa");
 }).then(result1 => {
   console.log("result1", result1);
-  return Promise.resolve("bbb");
+  return "bbb";
 }).then(result2 => {
   console.log("result2", result2);
+  return Promise.resolve("ccc");
+}).then(result3 => {
+  console.log("result3", result3);
+  return Promise((a, r) => {
+    setTimeout(()=>{
+      a("ddd");
+    },1000);
+  });
+}).then(result4 => {
+  console.log("result4", result4);
 });
 
 
